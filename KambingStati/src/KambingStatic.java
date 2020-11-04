4775050c165f81435049f64472a1f07ff090d817
sriutaminingsih <49768122+tamiutami@users.noreply.github.com>
2020-11-05 02:56:05 +0700

    Add files via upload

---
+++ KambingStati/src/KambingStatic.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author  @author Sri Utami Ningsih
+ * Nama  : Sri Utami Ningsih
+ * Kelas : PBO-2
+ * Nim   : 10116378
+ * 
+ */
+public class KambingStatic {
+    public static final String NAMA_KAMBING ="MIDUN";
+      public static void main(String[] args) {
+          Mamalia.jumlahKambing = 485000;
+          System.out.println(NAMA_KAMBING + " memilki jumlah kambing sebanyak " + Mamalia.jumlahKambing);
+      }
+}
+
---
+++ KambingStati/src/Mamalia.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Tami
+ */
+public class Mamalia {
+    public static int jumlahKambing;
+}
